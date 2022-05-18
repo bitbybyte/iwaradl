2a6096f4aab5fffda7f6a1edba7c28c1278e9b7d
bitbybyte <bittobaito@gmail.com>
2022-05-18 19:02:39 -0400

    Support multiple URLs as argument

--- iwaradl.py
+++ iwaradl.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
 
     cmdl_parser.add_argument("-q", "--quiet", action="store_true", dest="quiet", help="suppress output")
     cmdl_parser.add_argument("-v", "--version", action="version", version=cmdl_version)
-    cmdl_parser.add_argument("url", help="video URL")
+    cmdl_parser.add_argument("url", action="store", nargs="*", help="video URL")
 
     dl_group = cmdl_parser.add_argument_group("download options")
     dl_group.add_argument("-q", "--quality", nargs="?", default="Source", dest="quality", help="video quality to download")
@@ -37,18 +37,22 @@ if __name__ == "__main__":
 
     downloader = models.IwaraDownloader(dump_metadata=cmdl_opts.dump_metadata, save_thumbnail=cmdl_opts.save_thumbnail, filename_template=cmdl_opts.output_path, quiet=cmdl_opts.quiet)
 
-    url_groups = downloader.IWARA_URL_RE.match(cmdl_opts.url)
-    if url_groups:
-        if url_groups[2] == "videos":
-            downloader.download_video(url_groups[3], quality=cmdl_opts.quality)
-        elif url_groups[2] == "images":
-            # downloader.download_image(url_groups[3])
-            sys.exit("Not yet implemented")
-        elif url_groups[3] == "playlist":
-            # downloader.download_playlist(url_groups[3])
-            sys.exit("Not yet implemented")
-        elif url_groups[4] == "users":
-            # downloader.download_user(url_groups[3])
-            sys.exit("Not yet implemented")
+    if cmdl_opts.url:
+        for url in cmdl_opts.url:
+            url_groups = downloader.IWARA_URL_RE.match(url)
+            if url_groups:
+                if url_groups[2] == "videos":
+                    downloader.download_video(url_groups[3], quality=cmdl_opts.quality)
+                elif url_groups[2] == "images":
+                    # downloader.download_image(url_groups[3])
+                    sys.exit("Not yet implemented")
+                elif url_groups[3] == "playlist":
+                    # downloader.download_playlist(url_groups[3])
+                    sys.exit("Not yet implemented")
+                elif url_groups[4] == "users":
+                    # downloader.download_user(url_groups[3])
+                    sys.exit("Not yet implemented")
+            else:
+                print("URL is not fully qualified or invalid. Skipping...")
     else:
         sys.exit("Please provide a fully qualified Iwara URL")
